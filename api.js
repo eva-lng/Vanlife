417e229992dad19fcf2d7f5be192cc42126ca90a
Eva Langerova <langerova_eva@yahoo.com>
2024-07-25 12:35:21 +0200

    first commit

---
+++ api.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, 
+    collection, 
+    doc, 
+    getDocs, 
+    getDoc, 
+    query,
+    where
+} from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClqFMMnAzvPxu0FzFNbEKIvv_IVEHMago",
+  authDomain: "vanlife-1fdb4.firebaseapp.com",
+  projectId: "vanlife-1fdb4",
+  storageBucket: "vanlife-1fdb4.appspot.com",
+  messagingSenderId: "330130560433",
+  appId: "1:330130560433:web:dd08ed8502f5bb36659990"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+// export async function getHostVans(id) {
+//     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
